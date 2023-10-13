94c1aaa8a568ea46e364206d3cc5db661a4e252f
Aravind <aravindu28@gmail.com>
2023-10-13 13:15:35 +0530

    [UPDATE] updated index file

--- dgz_project_default_task/__manifest__.py
+++ dgz_project_default_task/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Project Default Task',
-    'version': '16.0.1',
+    'version': '16.0.2',
     'sequence': 1,
     'website': 'https://digitztech.com',
     'category': 'Extra Tools',
